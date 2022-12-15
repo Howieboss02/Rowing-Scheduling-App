7d16445d38e088d3e48fc6c4ec234856f76159e1
Macsim Violeta-Mara <V.M.Macsim@student.tudelft.nl>
2022-12-15 18:50:38 +0100

    Add API to notification

--- notification-microservice/src/main/java/nl/tudelft/sem/template/NotificationController.java
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/NotificationController.java
@@ -1,24 +1,45 @@
 package nl.tudelft.sem.template;
 
-import nl.tudelft.sem.template.Notification;
+import nl.tudelft.sem.template.services.EventService;
+import nl.tudelft.sem.template.services.UserService;
+import nl.tudelft.sem.template.shared.entities.*;
+import nl.tudelft.sem.template.shared.enums.Outcome;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/notification")
 public class NotificationController {
 
-  private Notification notification;
-  //private EventService eventService;
+  private final Notification notification;
+  private final EventService eventService;
+  private final UserService userService;
 
   @Autowired
-  public NotificationController(Notification notification){
+  public NotificationController(Notification notification, EventService eventService, UserService userService){
     this.notification = notification;
+    this.eventService = eventService;
+    this.userService = userService;
   }
 
-  //@PostMapping(path = "addNotification/{eventId}")
-  //public ResponseEntity<String> sendNotification(@PathVariable ("eventId") Long id, @RequestBody User user, @RequestBody Outcome outcome){
-    //Event event =
- // }
+  /**
+   * API for sending a notification for an event to a specific user
+   * @param idEvent the id of the event the user is getting notified
+   * @param idUser the id of the user we want to send the message
+   * @param outcome the outcome of enqueueing
+   * @return either a "NOT_FOUND" if the ids are invalid or the final message of the notification
+   */
+  @PostMapping(path = "addNotification/{eventId}")
+  public ResponseEntity<String> sendNotification(@PathVariable ("eventId") Long idEvent, @RequestBody long idUser, @RequestBody Outcome outcome){
+    Optional<Event> event = eventService.getById(idEvent);
+    if(event.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    Optional<User> user = userService.getById(idUser);
+    if(user.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return ResponseEntity.ok(notification.sendNotification(user.get(), event.get(), outcome));
+  }
 
 }
