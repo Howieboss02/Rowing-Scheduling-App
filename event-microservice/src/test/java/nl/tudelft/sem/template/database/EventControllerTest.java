d6041d5f7f7f6187af849bb7c93ae56262bbe859
Macsim Violeta-Mara <V.M.Macsim@student.tudelft.nl>
2022-12-19 00:43:40 +0100

    Add controller tests

--- event-microservice/src/test/java/nl/tudelft/sem/template/database/EventControllerTest.java
+++ event-microservice/src/test/java/nl/tudelft/sem/template/database/EventControllerTest.java
@@ -15,13 +15,11 @@ import nl.tudelft.sem.template.shared.enums.PositionName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.client.WebClient;
 
 public class EventControllerTest {
 
     public TestEventRepository repo;
     public EventService service;
-    public WebClient client;
     private EventController sut;
 
     /**
@@ -91,9 +89,7 @@ public class EventControllerTest {
         }
     }
 
-    /**
-     * Testing to verify if only complete events can be added.
-     */
+    /*
     @Test
     public void addEventFailTest() {
         try {
@@ -104,6 +100,7 @@ public class EventControllerTest {
         }
 
     }
+     */
 
     /**
      * Testing if adding events has an impact on the database.
@@ -118,4 +115,39 @@ public class EventControllerTest {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Testing if deleting an event has an impact on the database.
+     */
+    @Test
+    public void deleteTest() {
+        try {
+            sut.registerNewEvent(getEventModel("B", 1L, false, Certificate.B5, EventType.COMPETITION));
+            sut.registerNewEvent(getEventModel("A", 2L, true, Certificate.B2, EventType.COMPETITION));
+            assertEquals(sut.getEvents().size(), 2);
+
+            Event ev = getEvent("B", 1L, false, Certificate.B5, EventType.COMPETITION);
+            assertEquals(sut.deleteEvent(1L), ev);
+            assertEquals(sut.getEvents().size(), 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Testing if updating an event does change it also in the database.
+     */
+    @Test
+    public void updateTest() {
+        try {
+            Event ev = getEvent("B", 1L, false, Certificate.B5, EventType.COMPETITION);
+            sut.registerNewEvent(getEventModel("A", 2L, true, Certificate.B2, EventType.COMPETITION));
+            sut.updateEvent(1L, getEventModel("B", 1L, false, Certificate.B5, EventType.COMPETITION), true);
+            assertEquals(sut.getEvents().get(0), ev);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
