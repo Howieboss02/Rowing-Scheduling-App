92272ccc40bfa928aafe359f9683f535a83f11a0
Stanislaw Howard <S.M.Howard@student.tudelft.nl>
2022-12-20 10:36:17 +0100

    save progress

--- api-gateway/src/main/java/nl/tudelft/sem/template/controllers/GatewayController.java
+++ api-gateway/src/main/java/nl/tudelft/sem/template/controllers/GatewayController.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.controllers;
 
 
 import nl.tudelft.sem.template.services.GatewayService;
+import nl.tudelft.sem.template.shared.domain.TimeSlot;
 import nl.tudelft.sem.template.shared.models.AuthenticationRequestModel;
 import nl.tudelft.sem.template.shared.models.AuthenticationResponseModel;
 import nl.tudelft.sem.template.shared.models.RegistrationRequestModel;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.persistence.Convert;
 
 
 @RestController
@@ -41,4 +44,35 @@ public class GatewayController {
         System.out.println("get all notifications");
         return gatewayService.getAllNotifications(userId);
     }
+
+    @PostMapping(path = "/user/addNotification/{userId}")
+    public ResponseEntity addNotification(@PathVariable String userId, @RequestBody String notification) {
+        System.out.println("add notification");
+        return gatewayService.addNotification(userId, notification);
+    }
+
+    @PutMapping(path = "/user/schedule/addRecurring/{userId}")
+    public ResponseEntity addRecurring(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
+        System.out.println("add recurring");
+        return gatewayService.addRecurring(userId, timeSlot);
+    }
+
+    @PutMapping(path = "/user/schedule/removeRecurring/{userId}")
+    public ResponseEntity removeRecurring(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
+        System.out.println("remove recurring");
+        return gatewayService.removeRecurring(userId, timeSlot);
+    }
+
+    @PutMapping(path = "/user/schedule/include/{userId}")
+    public ResponseEntity includeTimeSlot(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
+        System.out.println("include time slot");
+        return gatewayService.includeTimeSlot(userId, timeSlot);
+    }
+    @PutMapping(path = "/user/schedule/exclude/{userId}")
+    public ResponseEntity removeTimeslot(@PathVariable String userId, @RequestBody TimeSlot timeslot) {
+        System.out.println("remove timeslot");
+        return gatewayService.removeTimeslot(userId, timeslot);
+    }
+
+
 }
--- api-gateway/src/main/java/nl/tudelft/sem/template/services/GatewayService.java
+++ api-gateway/src/main/java/nl/tudelft/sem/template/services/GatewayService.java
@@ -1,14 +1,21 @@
 package nl.tudelft.sem.template.services;
 
+import nl.tudelft.sem.template.shared.domain.Position;
 import nl.tudelft.sem.template.shared.enums.MicroservicePorts;
+import nl.tudelft.sem.template.shared.enums.Certificate;
+import nl.tudelft.sem.template.shared.domain.TimeSlot;
 import nl.tudelft.sem.template.shared.models.AuthenticationRequestModel;
 import nl.tudelft.sem.template.shared.models.AuthenticationResponseModel;
 import nl.tudelft.sem.template.shared.models.RegistrationRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.Transient;
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class GatewayService {
@@ -50,7 +57,7 @@ public class GatewayService {
      * Gets all users.
      */
     public ResponseEntity getAllUsers() {
-        return restTemplate.getForEntity(this.apiPrefix + MicroservicePorts.USER.port + userPath
+        return restTemplate.getForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
                 + "/all", ResponseEntity.class);
     }
 
@@ -58,16 +65,47 @@ public class GatewayService {
      * get all notifications for a user.
      */
     public ResponseEntity getAllNotifications(String userId) {
-        return restTemplate.getForEntity(this.apiPrefix + MicroservicePorts.USER.port + userPath
-                + "/getNotifications/{ userId }", ResponseEntity.class);
+        return restTemplate.getForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                + "/getNotifications/{" + userId + "}", ResponseEntity.class);
     }
 
+    /**
+     * add a notification for a user.
+     */
+    public ResponseEntity addNotification(String userId, String notification) {
+        return restTemplate.postForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                + "/addNotification/{" + userId + "}", notification, ResponseEntity.class);
+    }
 
     // TODO register user endpoint! Should it be done here or should it only be done by the authentication microservice?
 
-    public ResponseEntity deleteUser(String userId) {
-        return restTemplate.getForEntity(this.apiPrefix + MicroservicePorts.USER.port + userPath
-                + "/delete/{ userId }", ResponseEntity.class);
+//    public ResponseEntity deleteUser(String userId) {
+//        return restTemplate.delete(this.apiPrefix + MicroservicePorts.USER.port + userPath
+//                + "/delete/{" + userId + "}", ResponseEntity.class);
+//    }
+
+    public ResponseEntity updateUser(String userId, String name, String organization, String email, String gender, Certificate certificate, List<Position> positions) {
+        return restTemplate.put(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                + "/update/{" + userId + "}",  ,ResponseEntity.class);
     }
+
+    public ResponseEntity setName(String userId, String name) {
+        return restTemplate.postForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                        + "/name/{" + userId + "}", name, ResponseEntity.class);
+    }
+
+    public ResponseEntity setOrganization(String userId, String organization) {
+        return restTemplate.postForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                        + "/organization/{" + userId + "}", organization, ResponseEntity.class);
+    }
+
+    public ResponseEntity setCertificate(String userId, Certificate certificate) {
+        return restTemplate.postForObject(this.apiPrefix + MicroservicePorts.USER.port + userPath
+                        + "/certificate/{" + userId + "}", certificate, ResponseEntity.class);
+    }
+
+
+
+
 
 }
