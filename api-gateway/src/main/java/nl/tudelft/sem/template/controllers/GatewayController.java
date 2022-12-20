eea8f1cb894ce27982a38b3e080d7ddaac77a576
Stanisław Howard <S.M.Howard@student.tudelft.nl>
2022-12-20 15:24:33 +0100

    add try catch

--- api-gateway/src/main/java/nl/tudelft/sem/template/controllers/GatewayController.java
+++ api-gateway/src/main/java/nl/tudelft/sem/template/controllers/GatewayController.java
@@ -3,6 +3,7 @@ package nl.tudelft.sem.template.controllers;
 
 import nl.tudelft.sem.template.services.GatewayService;
 import nl.tudelft.sem.template.shared.domain.TimeSlot;
+import nl.tudelft.sem.template.shared.entities.User;
 import nl.tudelft.sem.template.shared.models.AuthenticationRequestModel;
 import nl.tudelft.sem.template.shared.models.AuthenticationResponseModel;
 import nl.tudelft.sem.template.shared.models.RegistrationRequestModel;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.Convert;
+import java.util.List;
 
 
 @RestController
@@ -20,6 +22,8 @@ public class GatewayController {
 
     @Autowired
     private transient GatewayService gatewayService;
+
+    // AUTHENTICATION MS
 
     @PostMapping(path = "/auth/register")
     public ResponseEntity register(@RequestBody RegistrationRequestModel request) {
@@ -33,46 +37,96 @@ public class GatewayController {
         return ResponseEntity.ok(gatewayService.login(request));
     }
 
+
+
+    // USER MS
+
     @GetMapping(path = "/user/all")
-    public ResponseEntity getAllUsers() {
+    public ResponseEntity<List<User>> getAllUsers() {
         System.out.println("get all users");
-        return gatewayService.getAllUsers();
+        try {
+            return ResponseEntity.ok((List<User>) gatewayService.getAllUsers());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping(path = "/user/getNotifications/{userId}")
-    public ResponseEntity getAllNotifications(@PathVariable String userId) {
+    public ResponseEntity<List<String>> getAllNotifications(@PathVariable String userId) {
         System.out.println("get all notifications");
-        return gatewayService.getAllNotifications(userId);
+        try {
+            return ResponseEntity.ok((List<String>) gatewayService.getAllNotifications(userId));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping(path = "/user/setName/{userId}")
+    public ResponseEntity setName(@PathVariable String userId, @RequestBody String name) {
+        System.out.println("set name");
+        try {
+            gatewayService.setName(userId, name);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping(path = "/user/addNotification/{userId}")
     public ResponseEntity addNotification(@PathVariable String userId, @RequestBody String notification) {
         System.out.println("add notification");
-        return gatewayService.addNotification(userId, notification);
+        try {
+            gatewayService.addNotification(userId, notification);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping(path = "/user/schedule/addRecurring/{userId}")
     public ResponseEntity addRecurring(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
         System.out.println("add recurring");
-        return gatewayService.addRecurring(userId, timeSlot);
+        try {
+            gatewayService.addRecurring(userId, timeSlot);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping(path = "/user/schedule/removeRecurring/{userId}")
     public ResponseEntity removeRecurring(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
         System.out.println("remove recurring");
-        return gatewayService.removeRecurring(userId, timeSlot);
+        try {
+            gatewayService.removeRecurring(userId, timeSlot);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping(path = "/user/schedule/include/{userId}")
     public ResponseEntity includeTimeSlot(@PathVariable String userId, @RequestBody TimeSlot timeSlot) {
         System.out.println("include time slot");
-        return gatewayService.includeTimeSlot(userId, timeSlot);
+        try {
+            gatewayService.includeTimeSlot(userId, timeSlot);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
     @PutMapping(path = "/user/schedule/exclude/{userId}")
     public ResponseEntity removeTimeslot(@PathVariable String userId, @RequestBody TimeSlot timeslot) {
         System.out.println("remove timeslot");
-        return gatewayService.removeTimeslot(userId, timeslot);
+        try {
+            gatewayService.removeTimeslot(userId, timeslot);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
+
+    // EVENT MS
 
 
 }
