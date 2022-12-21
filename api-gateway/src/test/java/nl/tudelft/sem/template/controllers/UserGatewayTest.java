36510d7d2ce51a4e1150d5be3a7ef5cc1a54e9f4
Stanisław Howard <S.M.Howard@student.tudelft.nl>
2022-12-21 01:14:46 +0100

    added Time to pair converter and test class for user gateway

---
+++ api-gateway/src/test/java/nl/tudelft/sem/template/controllers/UserGatewayTest.java
@@ -0,0 +1,4 @@
+package nl.tudelft.sem.template.controllers;
+
+public class UserGatewayTest {
+}
---
+++ shared/src/main/java/nl/tudelft/sem/template/shared/converters/TextTimeToMinutesConverter.java
@@ -0,0 +1,34 @@
+package nl.tudelft.sem.template.shared.converters;
+
+import javax.persistence.AttributeConverter;
+import org.springframework.data.util.Pair;
+
+// Converter from a Pair of Integers being minutes to a String
+// representing the time in the format HH:MM-HH:MM
+
+public class TextTimeToMinutesConverter implements AttributeConverter<Pair<Integer, Integer>, String> {
+    private static final String SPLIT_CHAR = "-";
+
+    @Override
+    public String convertToDatabaseColumn(Pair<Integer, Integer> time) {
+        if (time == null) {
+            return "";
+        }
+        return String.format("%02d:%02d", time.getFirst() / 60, time.getFirst() % 60)
+                + SPLIT_CHAR
+                + String.format("%02d:%02d", time.getSecond() / 60, time.getSecond() % 60);
+    }
+
+    @Override
+    public Pair<Integer, Integer> convertToEntityAttribute(String time) {
+        if (time == null || time.isEmpty()) {
+            return null;
+        }
+        String[] times = time.split(SPLIT_CHAR);
+        return Pair.of(Integer.parseInt(times[0].split(":")[0]) * 60
+                + Integer.parseInt(times[0].split(":")[1]),
+                Integer.parseInt(times[1].split(":")[0]) * 60
+                        + Integer.parseInt(times[1].split(":")[1]));
+    }
+
+}
